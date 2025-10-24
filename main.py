aa872a5e677a9de65bbf0a3c163300881db41af0
Lirnichenko_Andrii <22fmif.a.lirnichenko@std.npu.edu.ua>
2025-10-24 08:54:32 +0300

    Додано новий ключ для обчисоення середньої оцінки.

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ with open('dataset.csv', mode='r', encoding='utf-8') as file:
     reader =csv.DictReader(file)
     for row in reader:
         all_students.append(row)
+
+for student in all_students:
+    grades = [
+        float(student['Основи програмування']),
+        float(student['Лінійна алгебра']),
+        float(student['Проекційна геометрія']),
+        float(student['Математичний аналіз'])
+    ]
+    student['Середня оцінка'] = round(sum(grades) / len(grades), 2)
